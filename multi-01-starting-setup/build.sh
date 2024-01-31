3278724d3f00ee2977098903f63fee25c3b71110
jilanim <jilanim@sheridanc.on.ca>
2024-01-30 20:44:38 -0500

    remove everything

--- multi-01-starting-setup/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# check if docker network "goals-net" exists and if not then create it
-if [ -z $(docker network ls --filter name=goals-net -q) ]; then
-  docker network create goals-net
-fi
-
-docker build -t goals-backend ./backend/
-docker build -t goals-frontend ./frontend/
